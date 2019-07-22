81f4cce5ba7f04c6e1c73584ccfd4b9a04fac426
yes <yes@emq.com>
2019-07-22 15:13:34 +0800

    wip

--- crawler/src/national-park/test.js
+++ crawler/src/national-park/test.js
@@ -1,7 +1,22 @@
-// let huts = require('./huts.js')
-let beds = require('./beds.js')
+const getHuts = require("./getHuts");
+const getBeds = require("./getBeds");
 
-// 31 + 7
-// huts.get().then(res => console.log(res))
+// let beds = require('./beds.js')
 
-beds.get('七卡山莊').then(res => console.log(res));
+// beds.get('七卡山莊').then(res => console.log(res));
+
+test("Get sheipa huts", async () => {
+  const huts = await getHuts("sheipa");
+  expect(huts.length).toBe(31);
+});
+
+test("Get taroko huts", async () => {
+  const huts = await getHuts("taroko");
+  expect(huts.length).toBe(7);
+});
+
+test("Get sheipa beds", async () => {
+  const beds = await getBeds("七卡山莊");
+  console.log(beds);
+  expect(beds.length).toBe(31);
+});

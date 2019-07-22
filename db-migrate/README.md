88922c34e260e101b9a9ea5b938090034d7aeb63
yes <yes@emq.com>
2019-07-22 14:37:35 +0800

    wip

---
+++ db-migrate/README.md
@@ -0,0 +1,21 @@
+# Tech stack
+
+`db-migrate`
+
+# Create a new migrate
+
+```
+yarn run create
+```
+
+# Up migrate
+
+```
+yarn run up
+```
+
+or
+
+```
+docker-compose up db-migrate
+```
---
+++ db-migrate/migrations/sqls/20190722062312-migrate-up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE huts (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    nameEn text NOT NULL,
+    admin text NOT NULL,
+    url text NOT NULL,
+    size integer NOT NULL,
+    available boolean NOT NULL,
+    rooms integer,
+);
+

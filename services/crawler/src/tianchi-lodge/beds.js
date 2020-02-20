7adec38c2f317c20f9b4cc7eb75e0bb9c49d0bc0
Yes <joshuayes@gmail.com>
2020-02-21 02:10:28 +0800

    wip

--- services/crawler/src/tianchi-lodge/beds.js
+++ services/crawler/src/tianchi-lodge/beds.js
@@ -1,70 +1,81 @@
-let moment = require('moment');
-let cheerio = require('cheerio');
-let co = require('co');
-let rp = require('request-promise');
-let _ = require('lodash');
+const puppeteer = require("puppeteer");
 
-const huts = _.keyBy(require('./huts.json'), 'name');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
 
-const getBeds = co.wrap(function* (year, month) {
+  await browser.close();
+})();
 
-  let body, $, beds, reRemaining, reApplying;
+// let moment = require('moment');
+// let cheerio = require('cheerio');
+// let co = require('co');
+// let rp = require('request-promise');
+// let _ = require('lodash');
 
-  body = yield rp({
-    uri: "http://tconline.forest.gov.tw/order/",
-    qs: {
-      year,
-      month
-    },
-  });
-  $ = cheerio.load(body);
+// const huts = _.keyBy(require('./huts.json'), 'name');
 
-  reRemaining = new RegExp(/剩餘床位:(\d*)/);
-  reApplying = new RegExp(/目前報名:(\d*)/);
+// const getBeds = co.wrap(function* (year, month) {
 
-  beds = $('td.in_calendar_date').map( (i, el) => {
-    const table = $(el).closest('table');
-    const remaining = (reRemaining.exec(table.text()) || [])[1];
-    const applying = (reApplying.exec(table.text()) || [])[1];
-    if (remaining) {
-      return {
-        date: moment(`${year}-${month}-${table.find('font[color="red"]').text()}`, 'YYYY-M-DD').format('YYYY-MM-DD'),
-        remaining: Number.parseInt(remaining),
-        applying: Number.parseInt(applying)
-      }
-    }
-  }).get();
+//   let body, $, beds, reRemaining, reApplying;
 
-  return beds
-})
+//   body = yield rp({
+//     uri: "http://tconline.forest.gov.tw/order/",
+//     qs: {
+//       year,
+//       month
+//     },
+//   });
+//   $ = cheerio.load(body);
 
-exports.get = co.wrap(function* () {
-  const bedsThisMonth = yield getBeds(
-    moment().year(),
-    ('0' + (moment().month() + 1)).slice(-2)
-  );
-  const bedsNextMonth = yield getBeds(
-    moment().add(1, 'M').year(),
-    ('0' + (moment().add(1, 'M').month() + 1)).slice(-2)
-  );
-  const bedsNextNextMonth = yield getBeds(
-    moment().add(2, 'M').year(),
-    ('0' + (moment().add(2, 'M').month() + 1)).slice(-2)
-  );
+//   reRemaining = new RegExp(/剩餘床位:(\d*)/);
+//   reApplying = new RegExp(/目前報名:(\d*)/);
 
-  beds = bedsThisMonth.concat(bedsNextMonth, bedsNextNextMonth)
-  const bedsObject = _.keyBy(beds, 'date');
-  const dayCount = moment(beds[beds.length - 1].date).diff(moment(beds[0].date), 'day') + 1;
-  const firstDate = beds[0].date;
-  beds = [];
-  for (var i = 0; i < dayCount; i++) {
-    beds.push(
-      bedsObject[moment(firstDate).add(i, 'day').format('YYYY-MM-DD')] ||
-      {
-        date: moment(firstDate).add(i, 'day').format('YYYY-MM-DD'),
-        remaining: 0,
-        applying: 0
-      })
-  }
-  return beds
-})
+//   beds = $('td.in_calendar_date').map( (i, el) => {
+//     const table = $(el).closest('table');
+//     const remaining = (reRemaining.exec(table.text()) || [])[1];
+//     const applying = (reApplying.exec(table.text()) || [])[1];
+//     if (remaining) {
+//       return {
+//         date: moment(`${year}-${month}-${table.find('font[color="red"]').text()}`, 'YYYY-M-DD').format('YYYY-MM-DD'),
+//         remaining: Number.parseInt(remaining),
+//         applying: Number.parseInt(applying)
+//       }
+//     }
+//   }).get();
+
+//   return beds
+// })
+
+// exports.get = co.wrap(function* () {
+//   const bedsThisMonth = yield getBeds(
+//     moment().year(),
+//     ('0' + (moment().month() + 1)).slice(-2)
+//   );
+//   const bedsNextMonth = yield getBeds(
+//     moment().add(1, 'M').year(),
+//     ('0' + (moment().add(1, 'M').month() + 1)).slice(-2)
+//   );
+//   const bedsNextNextMonth = yield getBeds(
+//     moment().add(2, 'M').year(),
+//     ('0' + (moment().add(2, 'M').month() + 1)).slice(-2)
+//   );
+
+//   beds = bedsThisMonth.concat(bedsNextMonth, bedsNextNextMonth)
+//   const bedsObject = _.keyBy(beds, 'date');
+//   const dayCount = moment(beds[beds.length - 1].date).diff(moment(beds[0].date), 'day') + 1;
+//   const firstDate = beds[0].date;
+//   beds = [];
+//   for (var i = 0; i < dayCount; i++) {
+//     beds.push(
+//       bedsObject[moment(firstDate).add(i, 'day').format('YYYY-MM-DD')] ||
+//       {
+//         date: moment(firstDate).add(i, 'day').format('YYYY-MM-DD'),
+//         remaining: 0,
+//         applying: 0
+//       })
+//   }
+//   return beds
+// })
